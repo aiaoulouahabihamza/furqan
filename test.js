3dded43c5a6606f120eaef8fcc7cc83ed90a2f0e
aiaoulouahabihamza <aiaoulouahabihamza@gmail.com>
2026-09-16 11:51:53 +0100

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-fetch('http://localhost:3000/auth.js?v=4').then(r => r.text()).then(t => {
-  console.log('auth.js stars with:', t.substring(0, 15));
-});
-fetch('http://localhost:3000/fatwas/app.js?v=4').then(r => r.text()).then(t => {
-  console.log('fatwas/app.js stars with:', t.substring(0, 15));
-});
